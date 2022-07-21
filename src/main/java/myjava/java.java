9c605539e6dc5c844dd3ae2e9e4cc42d636eb9ec
challa-bhagyalakkshmi <challabhagyalakshmi2298@gmail.com>
2022-07-21 18:37:11 +0530

    added files

---
+++ src/main/java/myjava/java.java
@@ -0,0 +1,13 @@
+package myjava;
+
+public class java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("my third java project");
+	
+		
+	}
+
+}
